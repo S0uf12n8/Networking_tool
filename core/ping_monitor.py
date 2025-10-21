d34555e222a4e214f4b4e4dd504c7cc41737d1d0
S0uf12n8 <oulkadisoufiane69@gmail.com>
2025-10-21 08:20:28 +0100

    importing ness lib

--- core/ping_monitor.py
+++ core/ping_monitor.py
@@ -1,1 +1,27 @@
 from ping3 import ping
+import yaml
+from time import datetime
+import psutil
+import os
+import pathlib
+import csv
+
+config_path = "config/config.yaml"
+log_dir = "data/logs/"
+log_file = "data/logs/ping_log.csv"
+
+load_conifg():
+
+
+
+ping_host():
+
+
+run_mointor():
+
+
+
+log_result():
+
+
+
